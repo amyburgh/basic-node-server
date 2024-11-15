dce2f2b918c7f6b0a45a5cddd61d45763db94592
amyburgh <air.myburgh@gmail.com>
2024-11-15 17:20:00 +0200

    Fix the file serving in index.js

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+import { createServer } from 'http';
+import url from 'url';
+import fs from 'fs';
+import path from 'path';
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello World</h1>');
-  res.end();
+const hostname = 'localhost';
+const port = 8080;
+
+const type = {
+  '.html': 'text/html',
+  '.css': 'text/css',
+  '': 'text/plain',
+};
+
+const server = createServer((req, res) => {
+  let pathname = `.${req.url}`;
+  if (pathname === './') pathname = './index.html';
+
+  const ext = path.parse(pathname).ext;
+  if (ext === '') pathname += '.html';
+
+  fs.readFile(pathname, (err, data) => {
+    if (err) {
+      fs.readFile('./404.html', (err404, data404) => {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end(data404);
+      });
+    } else {
+      res.writeHead(200, { 'Content-Type': `${type[ext]}` });
+      res.end(data);
+    }
+  });
 });
 
-server.listen(8080, () => {
-  console.log('Server is running...');
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
---
+++ test.js
@@ -0,0 +1,10 @@
+const ops = {
+  one: 1,
+  two: 2,
+};
+
+const print = (...opts) => {
+  console.log('run', one);
+};
+
+print(ops);
